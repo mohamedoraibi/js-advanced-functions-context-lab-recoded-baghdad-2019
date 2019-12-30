a29dc99e71b6426b6a4763a6cc2daa8c027b1b4f
Mohammed Al-Oraibi <mohamed.b.oraibi@gmail.com>
2019-12-30 07:34:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 /* Your Code Here */
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
-
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
